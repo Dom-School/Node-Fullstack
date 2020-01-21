e60366486399c2701b30a97fc2f636960a8cbe7e
Dom-School <54898810+Dom-School@users.noreply.github.com>
2020-01-21 23:39:22 +0200

    Finished 1.9

--- src/index.js
+++ src/index.js
@@ -11,12 +11,22 @@ const Button = (props) => (
 
 const Statistics = (props) => {
 
+  if (props.good == 0 && props.neutral == 0 && props.bad == 0) {
+    return <p>No feedback given</p>
+  }
+
   const all = props.good + props.neutral + props.bad;
   const average = (all != 0) ? (props.good - props.bad) / all : 0;
   const positive = (all != 0) ? String(props.good / all * 100).concat("%") : 0;
 
   return (
     <div>
+      <h1>statistics</h1>
+
+      <Display value={props.good} text="good" />
+      <Display value={props.neutral} text="neutral" />
+      <Display value={props.bad} text="bad" />
+
       <Display value={all} text="all" />
       <Display value={average} text="average" />
       <Display value={positive} text="positive" />
@@ -42,11 +52,6 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
-      <h1>statistics</h1>
-
-      <Display value={good} text="good" />
-      <Display value={neutral} text="neutral" />
-      <Display value={bad} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
